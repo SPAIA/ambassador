9ce05e5ca0d96964850e34cf2afdfe9b753385da
Tom <tom@playstate.co.za>
2024-04-27 10:26:54 +0200

    updates

--- logger/main.py
+++ logger/main.py
@@ -12,6 +12,9 @@ import qwiic_bme280
 import sys
 import time
 
+# Global event to signal threads to stop
+stop_event = threading.Event()
+
 # Load environment variables
 load_dotenv()
 
@@ -34,17 +37,23 @@ else:
 class CameraManager:
     def __init__(self):
         self.camera = Picamera2()
-        config = self.camera.create_video_configuration(main={"size": (1280, 720)})
-        self.camera.configure(config)
         self.lock = threading.Lock()
-        self.camera.start()
+        self.low_res_config = self.camera.create_video_configuration(main={"size": (640, 480)})
+        self.high_res_config = self.camera.create_video_configuration(main={"size": (1920, 1080)})
+
+    def configure(self, config):
+        with self.lock:
+            self.camera.stop()
+            self.camera.configure(config)
+            self.camera.start()
 
     def capture_frame(self):
         with self.lock:
             return self.camera.capture_array()
 
-    def __del__(self):
-        self.camera.stop()
+    def stop(self):
+        with self.lock:
+            self.camera.stop()
 
 camera_manager = CameraManager()
 
@@ -72,59 +81,80 @@ def upload_file(url, headers, file_path, key):
         print(f"An error occurred: {e}")
 
 def capture_data():
-    now = datetime.now()
-    file_name = now.strftime("%Y-%m-%d_%H-%M-%S")
-    img_name = f"images/{file_name}.jpg"
-    data = [{"time": now.timestamp(), "temperature": mySensor.temperature_celsius if mySensor.connected else 0, "humidity": mySensor.humidity if mySensor.connected else 0, "media": file_name }]
-    generate_or_append_csv(data)
-    frame = camera_manager.capture_frame()
-    cv2.imwrite(img_name, frame)  # Assuming the frame is directly savable
-    upload_file("https://api.spaia.co.za/field/events", {"Authorization": f"Bearer {api_token}"}, 'data.csv', "events")
-    upload_file("https://api.spaia.co.za/media", {"Authorization": f"Bearer {api_token}"}, img_name, "file")
-    try:
-        os.remove(img_name)
-        os.remove('data.csv')
-    except OSError as e:
-        print(f"Error removing files: {e.strerror} - {e.filename}")
+    print("capture")
+    while not stop_event.is_set():
+        now = datetime.now()
+        file_name = now.strftime("%Y-%m-%d_%H-%M-%S")
+        img_name = f"images/{file_name}.jpg"
+        print("saving ".img_name)
+        try:
+            camera_manager.lock.acquire() 
+            camera_manager.configure(camera_manager.high_res_config)
+            frame = camera_manager.capture_frame()
+            cv2.imwrite(img_name, frame)  # Assuming the frame is directly savable
+            upload_file("https://api.spaia.co.za/media", {"Authorization": f"Bearer {api_token}"}, img_name, "file")
+            camera_manager.configure(camera_manager.low_res_config)
+        finally:
+            camera_manager.configure(camera_manager.low_res_config)
+            camera_manager.lock.release()
+        data = [{"time": now.timestamp(), "temperature": mySensor.temperature_celsius if mySensor.connected else 0, "humidity": mySensor.humidity if mySensor.connected else 0, "media": file_name }]
+        generate_or_append_csv(data)
+        upload_file("https://api.spaia.co.za/field/events", {"Authorization": f"Bearer {api_token}"}, 'data.csv', "events")
+    
+        try:
+            os.remove(img_name)
+            os.remove('data.csv')
+        except OSError as e:
+            print(f"Error removing files: {e.strerror} - {e.filename}")
 
 def motion_detection():
-    first_frame = None
     try:
-        while True:
-            frame = camera_manager.capture_frame()
-            if frame is None:
-                print("No frame captured.")
-                continue
+        # camera_manager.configure(camera_manager.low_res_config)
+        first_frame = None
+        while not stop_event.is_set():
+            try:
+                print("detecting...")
+                frame = camera_manager.capture_frame()
+                if frame is None:
+                    print("No frame captured.")
+                    continue  # Ensures the loop continues even if a frame isn't captured
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-            if first_frame is None:
-                first_frame = gray
-                print("Set first frame.")
-                continue
-
-            frame_delta = cv2.absdiff(first_frame, gray)
-            thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-            motion_detected = False
-            for contour in contours:
-                if cv2.contourArea(contour) < 200:
+                if first_frame is None:
+                    first_frame = gray
+                    print("Set first frame.")
                     continue
-                print("Motion detected!")
-                capture_data()  # This will capture and upload data
-                motion_detected = True
-                break  # Break after the first detection to avoid multiple captures
 
-            if motion_detected:
-                print("Pausing motion detection for 30 seconds...")
-                time.sleep(30)  # Pause for 30 seconds after capturing data
+                frame_delta = cv2.absdiff(first_frame, gray)
+                thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+                thresh = cv2.dilate(thresh, None, iterations=2)
+                contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                motion_detected = False
+                for contour in contours:
+                    if cv2.contourArea(contour) < 200:
+                        continue
+                    print("Motion detected!")
+                    capture_data()  # This will capture and upload data
+                    motion_detected = True
+                    break  # Break after the first detection to avoid multiple captures
+
+                if motion_detected:
+                    print("Pausing motion detection for 30 seconds...")
+                    time.sleep(30)  # Pause for 30 seconds after capturing data
+            except Exception as e:
+                print(f"Error during motion detection: {e}")
+
     except Exception as e:
-        print(f"Error in motion_detection: {e}")
+        print(f"Error in setting up motion detection: {e}")
+    finally:
+        print('Motion detection stopped')
+
 
 def socket_listener():
+    
     host = 'localhost'
     port = 9090
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -139,9 +169,29 @@ def socket_listener():
                         break
                     capture_data()
 
+
 def main():
-    threading.Thread(target=motion_detection).start()
-    threading.Thread(target=socket_listener).start()
+    # Start threads
+    motion_thread = threading.Thread(target=motion_detection)
+    capture_thread = threading.Thread(target=capture_data)
+    socket_thread = threading.Thread(target=socket_listener)
+
+    motion_thread.start()
+    # capture_thread.start()
+    # socket_thread.start()
+    try:
+        # Wait for user to indicate shutdown
+        input("Press Enter to stop...\n")
+    finally:
+        # Signal all threads to stop
+        stop_event.set()
+        
+        # Wait for all threads to complete
+        motion_thread.join()
+        # capture_thread.join()
+        # socket_thread.join()
+
+        print("All threads have been stopped gracefully.")
 
 if __name__ == '__main__':
     main()
